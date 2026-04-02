b9c703be7d22adb8e3c1cbb9ea389a17c7fc4fe2
charlowaema <charles.waema@gmail.com>
2026-04-02 12:38:39 +0300

    changed axios

--- student-frontend/src/axios.js
+++ student-frontend/src/axios.js
@@ -1,5 +1,13 @@
 import axios from 'axios'
 
+const instance = axios.create({
+    //baseURL: 'http://127.0.0.1:8000/api'
+     baseURL: 'https://student-crud-main-pqqmnl.free.laravel.cloud/api'  // 👈 update this
+})
+
+export default instance
+
+
 // const instance = axios.create({
 //     baseURL: 'http://127.0.0.1:8000/api'
 // })
@@ -7,9 +15,3 @@ import axios from 'axios'
 // export default instance
 
 // import axios from 'axios'
-
-const instance = axios.create({
-    baseURL: 'http://127.0.0.1:8000/api'
-})
-
-export default instance
