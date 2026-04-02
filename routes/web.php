867c4430bfc50ec2543a1d44acf79a219024f5b1
charlowaema <charles.waema@gmail.com>
2026-04-02 12:01:27 +0300

    second commit

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,12 @@
 <?php
 
-// use Illuminate\Support\Facades\Route;
+ use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-<<<<<<< HEAD
+Route::get('/', function () {
+    return view('welcome');
+});
 
 
-Route::get('/{any}', function () {
-    return view('app');
-})->where('any', '.*');
-=======
->>>>>>> 4df599e9ccbbfb1db9bb0e33394e5c3460ce7a2c
+// Route::get('/{any}', function () {
+//     return view('app');
+// })->where('any', '.*');
