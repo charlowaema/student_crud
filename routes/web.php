106472a3b04a0ec9cc4258b4a5ac30a93f91f305
charlowaema <charles.waema@gmail.com>
2026-04-02 12:07:33 +0300

    Uncomment route for app view and remove welcome route

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,12 @@
 <?php
 
- use Illuminate\Support\Facades\Route;
+//  use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 
-// Route::get('/{any}', function () {
-//     return view('app');
-// })->where('any', '.*');
+Route::get('/{any}', function () {
+    return view('app');
+})->where('any', '.*');
