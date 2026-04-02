3036baf35753cea834f4760b771f1e7be8126688
charlowaema <charles.waema@gmail.com>
2026-04-02 12:34:58 +0300

    Update axios.js

--- student-frontend/src/axios.js
+++ student-frontend/src/axios.js
@@ -1,15 +1,15 @@
 import axios from 'axios'
 
-// const instance = axios.create({
-//     baseURL: 'http://127.0.0.1:8000/api'
-// })
-
-// export default instance
-
-// import axios from 'axios'
-
 const instance = axios.create({
-    baseURL: 'https://student-crud-main-pqqmnl.free.laravel.cloud/api'
+    baseURL: 'http://127.0.0.1:8000/api'
 })
 
 export default instance
+
+// import axios from 'axios'
+
+// const instance = axios.create({
+//     baseURL: 'https://student-crud-main-pqqmnl.free.laravel.cloud/api'
+// })
+
+// export default instance
