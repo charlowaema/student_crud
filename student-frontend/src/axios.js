cef4767b489d97359ad78e2e47fbf843b9a48cd7
charlowaema <charles.waema@gmail.com>
2026-04-02 12:55:59 +0300

    Update axios.js

--- student-frontend/src/axios.js
+++ student-frontend/src/axios.js
@@ -9,7 +9,7 @@ import axios from 'axios'
 // import axios from 'axios'
 
 const instance = axios.create({
-    baseURL: 'https://student-crud-main-pqqmnl.free.laravel.cloud/'
+    baseURL: 'https://student-crud-main-pqqmnl.free.laravel.cloud/api'
 })
 
 export default instance
