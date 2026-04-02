ba0135128ee46d9d8cab4b3571c90b701611e075
charlowaema <charles.waema@gmail.com>
2026-04-02 12:18:32 +0300

    second commit

--- student-frontend/src/axios.js
+++ student-frontend/src/axios.js
@@ -1,7 +1,9 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL: 'http://127.0.0.1:8000/api'
+    //baseURL: 'http://127.0.0.1:8000/api'
+
+     baseURL: 'https://student-crud-main-pqqmnl.free.laravel.cloud/api'  // 👈 update this
 })
 
 export default instance
